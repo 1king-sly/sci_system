ea86dfec320feab57b21554b88fb43a261e9dae9
ClarenceLaria <clarencelaria@gmail.com>
2024-02-29 17:24:24 +0300

    MembersCard

--- app/(pages)/Staff/page.tsx
+++ app/(pages)/Staff/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CODCard from '@/app/(ui)/Component/CODCard'
+import MembersCard from '@/app/(ui)/MembersCard'
 
 export default function page() {
   return (
-    <div className='overflow-hidden'>
-        <h1 className='text-4xl font-serif font-bold max-[425px]:text-2xl'>About Computer Science</h1>
+    <div className='mx-20 my-10 overflow-hidden'>
+        <h1 className='text-4xl font-serif font-bold max-[425px]:text-2xl'>Staff Page</h1>
         <CODCard></CODCard>
+        <h1 className='text-4xl font-serif font-bold max-[425px]:text-2xl'>Memebers of Staff</h1>
+        <MembersCard></MembersCard>
     </div>
   )
 }
--- app/(ui)/Component/CODCard.tsx
+++ app/(ui)/Component/CODCard.tsx
@@ -9,7 +9,7 @@ export default function CODCard() {
     <Image src={COD} alt='Male Rep' height={300} width={400} className='w-full h-full object-cover border rounded-lg'></Image>
     </div>
     <div className='mr-40 w-full my-auto'>
-    <h1 className='text-4xl font-serif font-bold max-[425px]:text-2xl'>About Computer Science</h1>
+    <h1 className='text-4xl font-serif font-bold max-[425px]:text-2xl'>About Computer Science Department</h1>
     <p className=''>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque quis dicta vitae reprehenderit fugit. Illo omnis, odit enim debitis excepturi sunt assumenda praesentium quisquam! Ipsam quidem asperiores error beatae totam.</p>
     </div>
     </div>
