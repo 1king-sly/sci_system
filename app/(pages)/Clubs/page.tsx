3173fb66b9faae3dec46dd7755593cd1f2d03767
ClarenceLaria <clarencelaria@gmail.com>
2024-03-01 14:52:07 +0300

    Clubs cover

--- app/(pages)/Clubs/page.tsx
+++ app/(pages)/Clubs/page.tsx
@@ -5,11 +5,11 @@ import Club from '@/public/club.jpg'
 export default function page() {
   return (
     <div className=' my-4 overflow-hidden'>
-        <div className='w-screen relative h-[40vh] bg-gray-50  text-center font-'>
+        <div className='w-screen relative h-[40vh] bg-gray-50  text-center font-serif'>
             <Image src={Club} alt='Club' className='object-cover h-full w-full absolute'></Image>
             <div className='my-20 items-center justify-center absolute  w-full h-full text-white '>
-            <h1 className='text-2xl '>COMPARE, CHOOSE, ENJOY</h1>
-            <h1 className='text-4xl'>Our top SCI clubs</h1>
+              <h1 className='text-xl font'>COMPARE, CHOOSE, ENJOY</h1>
+              <h1 className='text-6xl font-bold'>Our top SCI <br />clubs</h1>
             </div>
         </div>
     </div>
--- app/error.tsx
+++ app/error.tsx
@@ -13,9 +13,9 @@ export default function Error({
     console.error(error);
   }, [error])
   return (
-    <div className='items-center justify-center'>
-        <h2>Something Went wrong!</h2>
-        <button onClick={() => reset()} className='border rounded-lg bg-sky-400'>Try again</button> 
+      <div className='my-20 mx-auto text-center text-xl'>
+        <h2 className='my-5'>Something Went wrong!</h2>
+        <button onClick={() => reset()} className='border rounded-lg bg-sky-400 p-2'>Try again</button> 
     </div>
   )
 }
