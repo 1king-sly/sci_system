3379f6bc283e95e8d2b956bf99cc8c472f755aef
nabiswavalencia <113972833+nabiswavalencia@users.noreply.github.com>
2024-03-02 16:52:51 +0300

    events page

---
+++ lence/src/Components/Sidebar.tsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <ul className="grid justify-between pl-20 gap-8">
+        <li>Upcoming Events</li>
+        <li>General Announcements</li>
+        <li>GDSC Club Activities</li>
+        <li>Health IT Activities</li>
+        <li>ACS Activities</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
