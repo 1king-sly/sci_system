ef000baafef2ce62c69534915c6af5d962f5ece3
ClarenceLaria <clarencelaria@gmail.com>
2024-03-05 21:32:38 +0300

    Updated Events Page

---
+++ app/(pages)/events/component/ACSEvents.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function ACSEvents() {
+  return (
+    <div id="acs" className="py-5">
+    <h2 className="font-bold mb-4">ACS ACTIVITIES</h2>
+     <div className="flex shadow-md p-3 border-t-2 m-2 hover:shadow-lg items-center gap-10 py-4">
+    <div className="border rounded-full p-6 bg-indigo-50" >
+    <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5zM5 8h14V6H5zm0 0V6zm2 6v-2h10v2zm0 4v-2h7v2z"/></svg>
+
+    </div>
+    <div>
+       <h3 className="font-semibold text-md">Event One</h3>
+    <p>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quidem sunt magnam earum asperiores error sed aut odio architecto voluptatibus tempora at autem, itaque perferendis maiores. Corporis qui non ex?
+   </p>
+    </div>
+ 
+     </div>
+     <div className="flex shadow-md p-3 border-t-2 m-2 hover:shadow-lg items-center gap-10 py-4">
+    <div className="border rounded-full p-6 bg-indigo-50" >
+    <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5zM5 8h14V6H5zm0 0V6zm2 6v-2h10v2zm0 4v-2h7v2z"/></svg>
+
+    </div>
+    <div>
+       <h3 className="font-semibold text-md">Event One</h3>
+    <p>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quidem sunt magnam earum asperiores error sed aut odio architecto voluptatibus tempora at autem, itaque perferendis maiores. Corporis qui non ex?
+   </p>
+    </div>
+ 
+     </div>
+     <div className="flex shadow-md p-3 border-t-2 m-2 hover:shadow-lg items-center gap-10 py-4">
+    <div className="border rounded-full p-6 bg-indigo-50" >
+    <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5zM5 8h14V6H5zm0 0V6zm2 6v-2h10v2zm0 4v-2h7v2z"/></svg>
+
+    </div>
+    <div>
+       <h3 className="font-semibold text-md ">Event One</h3>
+    <p>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quidem sunt magnam earum asperiores error sed aut odio architecto voluptatibus tempora at autem, itaque perferendis maiores. Corporis qui non ex?
+   </p>
+    </div>
+ 
+     </div>
+    </div>
+    )
+}
--- app/(pages)/events/page.tsx
+++ app/(pages)/events/page.tsx
@@ -4,22 +4,24 @@ import Upcoming from './component/Upcoming'
 import UpcomingSection from './component/UpcomingSection'
 import GeneralSection from './component/GeneralSection'
 import GDSCEvents from './component/GDSCEvents'
+import HealthITEvents from './component/HealthITEvents'
+import ACSEvents from './component/ACSEvents'
 
 export default function page() {
   return (
     <>
-        <div className="col-span-3 border-l-2">
+      <div className="grid grid-cols-4 py-4">
+        <div className="col-span-4 border-l-2">
           <div className=" static border-l-2  pl-10">
             <Upcoming></Upcoming>
             <UpcomingSection></UpcomingSection>
             <GeneralSection></GeneralSection>
             <GDSCEvents></GDSCEvents>
+            <HealthITEvents></HealthITEvents>
+            <ACSEvents></ACSEvents>
           </div>
-          </div>
-        
-        
-        
-
+        </div>
+      </div>  
     </>
   )
 }
