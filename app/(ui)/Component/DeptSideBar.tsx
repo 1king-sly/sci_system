4770b17d37b764131ec06f7c4e5ccdcfeb6e9f16
ClarenceLaria <clarencelaria@gmail.com>
2024-03-05 11:26:54 +0300

    Scroll Animation on DeptSideBar

--- app/(ui)/Component/DeptSideBar.tsx
+++ app/(ui)/Component/DeptSideBar.tsx
@@ -7,6 +7,12 @@ import React from 'react'
 import { useState } from 'react';
 
 export default function DeptSideBar() {
+    const scrollToSection = (id:string) => {
+        const element = document.getElementById(id);
+        if(element){
+            element.scrollIntoView({behavior:'smooth'})
+        }
+    }
 
     const pathName = usePathname();
     const [visible, SetVisible] = useState(false);
@@ -17,19 +23,19 @@ export default function DeptSideBar() {
     
     const links = [
         {
-        name:'Introduction',href:'#Introduction',
+        name:'Introduction',href:'/department#Introduction',id:'Introduction',
     },
         {
-        name:'Programs',href:'#Programs',
+        name:'Programs',href:'/department#Programs',id:'Programs',
     },
         {
-        name:'Mission and Vision',href:'#Mission',
+        name:'Mission and Vision',href:'/department#Mission',id:'Mission',
     },
         {
-        name:'Staff',href:'#Staff',
+        name:'Staff',href:'/department#Staff',id:'Staff',
     },
         {
-        name:'Student Representatives',href:'#Student-Representatives',
+        name:'Student Representatives',href:'/department#Student-Representatives',id:'Student-Representatives',
     },
 ]
   return (
@@ -45,14 +51,13 @@ export default function DeptSideBar() {
           
                     return (
                       <div>
-                      <Link
-                          className={clsx(`w-full px-2 py-1 rounded-md flex h-auto items-center justify-center text-sm hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 gap-2 text-gray-800`, {'bg-sky-100 text-blue-600': pathName === link.href,})}
+                      <div
+                          className={clsx(`cursor-pointer w-full px-2 py-1 rounded-md flex h-auto items-center justify-center text-sm hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 gap-2 text-gray-800`, {'bg-sky-100 text-blue-600': pathName === link.href,})}
                           key={link.name}
-                          href={link.href}
                       >
                           <div className='flex-col'>
-                              <div className='flex gap-2'>
-                                <h6 className="mb-2 mr-4 text-sm font-bold  tracking-wider uppercase">{link.name}</h6>{/*mr-4*/}
+                              <div className='flex gap-2' onClick={() => scrollToSection(link.id)}>
+                              <a className="mb-2 mr-4 text-sm font-bold  tracking-wider uppercase" key={link.name} onClick={() => scrollToSection(link.id)}>{link.name}</a>
                                 <ChevronDownIcon className='mb-2 w-6 max-[425px]:w-4 ml-16' onClick={toggleVisible} ></ChevronDownIcon>
                               </div>
                               <div className={clsx(`text-center`,!visible && 'hidden')}>
@@ -61,19 +66,19 @@ export default function DeptSideBar() {
                                 <h1 className=' mb-3 mr-4 border-2 p-2 rounded-lg hover:bg-gray-900' id='C.Forensics'>Computer Forensics</h1>
                                 <h1 className=' mb-3 mr-4 border-2 p-2 rounded-lg hover:bg-gray-900' id='C.Forensics'>Msc. Computer Science</h1></div>
                           </div>
-                      </Link>
+                      </div>
                       </div>
                     );
                   }
         return (
-                <Link
-                    className={clsx(`w-full rounded-md flex  items-center justify-between text-sm hover:bg-sky-100 hover:text-blue-600 md:p-2 md:px-3 gap-2`, {
+                <div
+                    className={clsx(`cursor-pointer w-full rounded-md flex  items-center justify-between text-sm hover:bg-sky-100 hover:text-blue-600 md:p-2 md:px-3 gap-2`, {
                         'bg-sky-100 text-blue-600': pathName === link.href,
                     })}
-                    href={link.href}
+                    key={link.name} onClick={() => scrollToSection(link.id)}
                 >
-                    <h6 className="mb-2 mr-4 text-sm font-bold  tracking-wider uppercase">{link.name}</h6>{/*mr-4*/}
-                </Link>
+                    <a className="mb-2 mr-4 text-sm font-bold  tracking-wider uppercase" key={link.name} onClick={() => scrollToSection(link.id)}>{link.name}</a>
+                </div>
         );
     })}
 {/* </div> */}
