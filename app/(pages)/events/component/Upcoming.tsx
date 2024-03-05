4a27925b35d9177f0dbb92b99197cbefb12644fa
ClarenceLaria <clarencelaria@gmail.com>
2024-03-05 21:24:48 +0300

    Updated Events Page

---
+++ app/(pages)/events/component/Upcoming.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Upcoming() {
+  return (
+    <div id="upcoming" className="text-left px-4 pb-10 bg-sky-400 bg-opacity-10 text-base/7">
+        <h2 className="font-bold text-3xl">EVENTS PAGE</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae laborum optio laboriosam beatae officiis, ab iure, exercitationem quaerat excepturi fugiat neque enim porro odio numquam sed quia aliquid libero voluptatem?</p>
+    </div> 
+    )
+}
