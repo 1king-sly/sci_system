1459175c2457a95760f4ee6154f9b8c0fd4c5869
ClarenceLaria <clarencelaria@gmail.com>
2024-03-05 10:44:25 +0300

    DropDown DeptSideBar

--- app/(ui)/Component/DeptSideBar.tsx
+++ app/(ui)/Component/DeptSideBar.tsx
@@ -41,6 +41,30 @@ export default function DeptSideBar() {
     <div className="text-center  mb-4 items-center justify-center gap-2">
 
     {links.map((link) => {
+                if (link.name === 'Programs') {
+          
+                    return (
+                      <div>
+                      <Link
+                          className={clsx(`w-full px-2 py-1 rounded-md flex h-auto items-center justify-center text-sm hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 gap-2 text-gray-800`, {'bg-sky-100 text-blue-600': pathName === link.href,})}
+                          key={link.name}
+                          href={link.href}
+                      >
+                          <div className='flex-col'>
+                              <div className='flex gap-2'>
+                                <h6 className="mb-2 mr-4 text-sm font-bold  tracking-wider uppercase">{link.name}</h6>{/*mr-4*/}
+                                <ChevronDownIcon className='mb-2 w-6 max-[425px]:w-4 ml-16' onClick={toggleVisible} ></ChevronDownIcon>
+                              </div>
+                              <div className={clsx(`text-center`,!visible && 'hidden')}>
+                                <h1 className=' mb-3 mr-4 border-2 p-2 rounded-lg hover:bg-gray-900' id='C.Science' >Bsc. Computer Science</h1>
+                                <h1 className=' mb-3 mr-4 border-2 p-2 rounded-lg hover:bg-gray-900' id='ETS' >ETS</h1>
+                                <h1 className=' mb-3 mr-4 border-2 p-2 rounded-lg hover:bg-gray-900' id='C.Forensics'>Computer Forensics</h1>
+                                <h1 className=' mb-3 mr-4 border-2 p-2 rounded-lg hover:bg-gray-900' id='C.Forensics'>Msc. Computer Science</h1></div>
+                          </div>
+                      </Link>
+                      </div>
+                    );
+                  }
         return (
                 <Link
                     className={clsx(`w-full rounded-md flex  items-center justify-between text-sm hover:bg-sky-100 hover:text-blue-600 md:p-2 md:px-3 gap-2`, {
