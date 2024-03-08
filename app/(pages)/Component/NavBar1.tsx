37f9faeebc2ec2c05fc3d1bda33f9dca84cc73b5
ClarenceLaria <clarencelaria@gmail.com>
2024-03-08 10:52:49 +0300

    Responsive NavBar

--- app/(pages)/Component/NavBar1.tsx
+++ app/(pages)/Component/NavBar1.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image';
-import React from 'react';
+import React, { use } from 'react';
 import logo from '@/public/logo.png'
 import Link from 'next/link';
 import clsx from 'clsx';
@@ -10,47 +10,109 @@ import AuthForm from './AuthForm';
 
 export default function NavBar1() {
   const pathname = usePathname();
+  const [viewAuth, setViewAuth] = useState(false);
   const [visible, setVisible] = useState(false);
   const toggleVisible = () => {
     setVisible((prevVisible) => !prevVisible);
   }
+  const toggleAuthForm = () => {
+    setViewAuth((prev) => !prev);
+  }
+  const closeAuthMenu = () =>{
+    setViewAuth(!toggleAuthForm);
+  }
   return (
     <>
-    <div className='w-full flex items-center h-[30vh] md:h-auto font-serif z-50 fixed bg-gray-900 '>
-      <div className='w-full h-full mx-auto py-5 px-4 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 flex items-center'>
+    <div className='w-full flex items-center h-[20vh]  font-serif z-50 fixed bg-gray-900 max-[425px]:hidden'>
+      <div className='w-full h-full mx-auto py-5 px-4  md:px-24 lg:px-8 flex items-center'> {/*sm:max-w-xl md:max-w-full lg:max-w-screen-xl*/}
 
         <div className='flex justify-start w-1/3 cursor-pointer'>
           <Link href='/'>
-            <Image src={logo} alt='Logo'  className='cursor-pointer '></Image>
+            <Image src={logo} alt='Logo'  className='cursor-pointer ' onClick={closeAuthMenu}></Image>
           </Link>
         </div>
         <div className='gap-4 flex-1 justify-evenly hidden text-sm text-white space-x-8 lg:flex'>
         <Link href="/"> 
-          <h1 className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/'})}>HOME</h1>
+          <h1 onClick={closeAuthMenu} className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/'})}>HOME</h1>
         </Link>
         <Link href="/department">
-          <h1 className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/department'})}>DEPARTMENT</h1>
+          <h1 onClick={closeAuthMenu} className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/department'})}>DEPARTMENT</h1>
         </Link>
         <Link href="/club">
-          <h1 className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname==='/club'})}>CLUBS</h1>
+          <h1 onClick={closeAuthMenu} className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname==='/club'})}>CLUBS</h1>
         </Link>
         <Link href="/events">
-          <h1 className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/events'})}>EVENTS</h1>
+          <h1 onClick={closeAuthMenu} className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/events'})}>EVENTS</h1>
         </Link>
         <Link href="/research">
-          <h1 className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/research'})}>RESEARCH</h1>
+          <h1 onClick={closeAuthMenu} className={clsx(`cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full`,{'after:w-full': pathname === '/research'})}>RESEARCH</h1>
         </Link>
         {/* <Link href="#"> */}
           <h1 className='cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-white after:block after:duration-500 hover:after:w-full'
-          onClick={toggleVisible}
+          onClick={toggleAuthForm}
           >LOGIN</h1>
         {/* </Link> */}
         </div>
       </div>
     </div>
-    <div className={clsx(`my-[25vh] fixed top-0 left-0 w-full h-full flex items-start justify-center bg-transparent z-50`, !visible && 'hidden')}>
+    <div className={clsx(`my-[25vh] fixed top-0 left-0 w-full h-full flex items-start justify-center bg-transparent z-50 max-[425px]:hidden`, !viewAuth && 'hidden')}>
     <AuthForm></AuthForm>
     </div>
+
+    {/* Mobile NavBar */}
+    <div className="lg:hidden ">
+          {/* Mobile menu dropdown */}
+          <div className="absolute top-0 left-0 w-full">
+            <div className="p-5 bg-gray-900 border rounded shadow-sm">
+              <div className="flex items-center justify-between mb-4">
+                <div>
+                  <Link href="/" className="inline-flex items-center">
+                    <Image src={logo} alt='Logo'  className='cursor-pointer '></Image>
+                  </Link> 
+                </div>
+                <div>
+                <button title="Open Menu" className={clsx(`p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline`, visible && `hidden`)}
+                onClick={toggleVisible}>
+                  <svg className="w-5 text-white-600" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
+                    <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
+                    <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
+                  </svg>
+                </button>
+                  <button aria-label="Close Menu" title="Close Menu" className={clsx(`p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline`, !visible && `hidden`)}
+                  onClick={toggleVisible}
+                  >
+                    <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+
+              <nav className={clsx(`w-full h-[480px] flex flex-col items-center justify-center bg-slate-900 z-50 relative p-10 rounded-md transition-all ease-in-out duration-300 `, !visible && 'hidden')}>
+                <ul className="space-y-4 z-[1000]">
+                  <li><Link href="/" className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">HOME</Link> </li>    
+                  <li><Link href="/department" className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">DEPARTMENT</Link> </li>
+                  <li><Link href="/club" className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">CLUBS</Link> </li>
+                  <li><Link href="/events" className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">EVENTS</Link> </li>
+                  <li><Link href="/research" className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">RESEARCH</Link> </li>
+                  <li>
+                    <Link
+                      href="/"
+                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    >
+                      Login
+                    </Link> 
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
