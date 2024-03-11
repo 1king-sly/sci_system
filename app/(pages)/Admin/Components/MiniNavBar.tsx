f0d27ff7a1c760133d72df6db1fd4b8d9faef9ee
ClarenceLaria <clarencelaria@gmail.com>
2024-03-11 11:30:30 +0300

    added the visibility function on the EventsDashboard

--- app/(pages)/Admin/Components/MiniNavBar.tsx
+++ app/(pages)/Admin/Components/MiniNavBar.tsx
@@ -7,19 +7,19 @@ export default function MiniNavBar() {
         <div className="fixed w-full z-50 flex justify-between px-10 py-1 bg-sky-200">
             <div><p>Hello UserName</p></div>
             <div className="flex gap-6"> 
-            <Link href="/">
+            <Link href="/Admin/addEvents">
                 <div className="flex items-center mx-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#07aff8" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"/></svg>
                 <p>Create Event</p>
             </div> 
             </Link>
-            <Link href="/">
+            <Link href="/Admin/Analytics">
                 <div className="flex items-center mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><rect width="32" height="32" fill="none"/><path fill="currentColor" d="M4 2H2v26a2 2 0 0 0 2 2h26v-2H4Z"/><path fill="currentColor" d="M30 9h-7v2h3.59L19 18.59l-4.29-4.3a1 1 0 0 0-1.42 0L6 21.59L7.41 23L14 16.41l4.29 4.3a1 1 0 0 0 1.42 0l8.29-8.3V16h2Z"/></svg>  
                     <p>Analytics</p>
                 </div>
             </Link>
-            <Link href="/">
+            <Link href="/Admin/EventsDashboard">
                 <div className="flex items-center mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 36 36"><rect width="36" height="36" fill="none"/><path fill="currentColor" d="m25.18 12.32l-5.91 5.81a3 3 0 1 0 1.41 1.42l5.92-5.81Z" className="clr-i-outline clr-i-outline-path-1"/><path fill="currentColor" d="M18 4.25A16.49 16.49 0 0 0 5.4 31.4l.3.35h24.6l.3-.35A16.49 16.49 0 0 0 18 4.25m11.34 25.5H6.66a14.43 14.43 0 0 1-3.11-7.84H7v-2H3.55A14.41 14.41 0 0 1 7 11.29l2.45 2.45l1.41-1.41l-2.43-2.46A14.41 14.41 0 0 1 17 6.29v3.5h2V6.3a14.47 14.47 0 0 1 13.4 13.61h-3.48v2h3.53a14.43 14.43 0 0 1-3.11 7.84" className="clr-i-outline clr-i-outline-path-2"/><path fill="none" d="M0 0h36v36H0z"/></svg>  
                     <p>Dashboard</p>
--- app/(pages)/Admin/EventsDashboard/page.tsx
+++ app/(pages)/Admin/EventsDashboard/page.tsx
@@ -1,7 +1,34 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import {useState} from 'react'
+import clsx from 'clsx';
 
 export default function page() {
+   const [visible, setVisible] = useState(true);
+   const [visibleDraft, setVisibleDraft] = useState(false);
+   const [visibleCompleted, setVisibleCompleted] = useState(false);
+
+   const toggleLive = () => {
+      setVisible((prev) => !prev)
+   }
+   const closeLive = () =>{
+      setVisible(!toggleLive);
+    }
+
+    const toggleDraft = () => {
+      setVisibleDraft((preview) => !preview)
+   }
+   const closeDraft = () =>{
+      setVisibleDraft(!toggleLive);
+    }
+
+    const toggleCompleted = () => {
+      setVisibleCompleted((prevC) => !prevC)
+   }
+   const closeCompleted = () =>{
+      setVisibleCompleted(!toggleLive);
+    }
   return (
     <div className='w-full '>
         <div className=" bg-sky-100 h-full">
@@ -11,10 +38,26 @@ export default function page() {
  </div>
  <div className="flex justify-between items-center px-10 py-10">
    <ul className="flex gap-4"> 
-    <li  id="live-btn">Live</li>
-    <li  id="draft-btn">Draft</li>
-    <li  id="completed-btn">Completed</li>
-    <li id="cancelled-btn">Canceled</li>
+    <li  id="live-btn" className='cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-black after:block after:duration-500 hover:after:w-full' 
+    onClick={() => {
+      toggleLive();
+      closeCompleted();
+      closeDraft();
+    }}
+    >Live</li>
+    <li  id="draft-btn" className='cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-black after:block after:duration-500 hover:after:w-full' 
+    onClick={() => {
+      toggleDraft();
+      closeCompleted();
+      closeLive();
+    }}>Draft</li>
+    <li  id="completed-btn" className='cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-black after:block after:duration-500 hover:after:w-full' 
+    onClick={() => {
+      toggleCompleted();
+      closeDraft();
+      closeLive();
+    }}>Completed</li>
+    <li id="cancelled-btn" className='cursor-pointer after:content-[""] after:w-0 after:h-0.5 after:m-auto after:bg-black after:block after:duration-500 hover:after:w-full'>Canceled</li>
    </ul>
    <Link href="/Admin/addEvents">
    <button className="flex items-center px-2 py-2 bg-slate-800 rounded text-white">
@@ -25,8 +68,8 @@ export default function page() {
 
   <div className="bg-white mx-10 h-fit rounded shadow-sm ">
   
-    <main id="main-content" className="flex-grow overflow-auto px-4 py-4">
-      <div id="live-content">  { /*<!-- live event -->*/}
+    <div id="main-content" className="flex-grow overflow-auto px-4 py-4">
+      <div id="live-content" className={clsx(``,!visible && 'hidden')}>  { /*<!-- live event -->*/}
         <div className="flex shadow-md p-3 border-t-2 m-2 hover:shadow-lg items-center gap-10 py-4">
           <div className="border rounded-full p-6 bg-indigo-50" >
           <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5zM5 8h14V6H5zm0 0V6zm2 6v-2h10v2zm0 4v-2h7v2z"/></svg>
@@ -54,21 +97,21 @@ export default function page() {
              </div>   
            
       </div>
-      <div id="draft-content" className='hidden'>
+      <div id="draft-content" className={clsx(``,!visibleDraft && 'hidden')}>
         <div className="flex shadow-md p-3 border-t-2 m-2 hover:shadow-lg items-center gap-10 py-4">
           <div className="border rounded-full p-6 bg-indigo-50" >
           <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5zM5 8h14V6H5zm0 0V6zm2 6v-2h10v2zm0 4v-2h7v2z"/></svg>
  
           </div>
           <div>
-             <h3 className="font-semibold text-md ">Event One</h3>
+             <h3 className="font-semibold text-md ">Draft One</h3>
           <p>
              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quidem sunt magnam earum asperiores error sed aut odio architecto voluptatibus tempora at autem, itaque perferendis maiores. Corporis qui non ex?
          </p>
           </div>
            </div>
       </div>
-      <div id="completed-content" className='hidden'>
+      <div id="completed-content" className={clsx(``,!visibleCompleted && 'hidden')}>
         <div className="flex shadow-md p-3 border-t-2 m-2 hover:shadow-lg items-center gap-10 py-4">
           <div className="border rounded-full p-6 bg-indigo-50" >
           <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5zM5 8h14V6H5zm0 0V6zm2 6v-2h10v2zm0 4v-2h7v2z"/></svg>
@@ -106,7 +149,7 @@ export default function page() {
               </div>
                </div>    
       </div>
-    </main>
+    </div>
   </div>
 
   </div>
