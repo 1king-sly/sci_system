4f92f399fff87205a4c622850b67cc954c8b95cc
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-03-19 16:33:21 +0300

    added dompurify for security

--- app/(pages)/blog/[id]/page.tsx
+++ app/(pages)/blog/[id]/page.tsx
@@ -4,6 +4,11 @@ import Image from 'next/image'
 import Pic from '@/public/ProfilePic.jpeg'
 import Banner from '@/public/pexels-pixabay-33045.jpg'
 import { fetchSingleBlog } from '@/app/lib/actions'
+import { JSDOM } from "jsdom";
+import DOMPurify from 'dompurify'
+
+const window = new JSDOM("").window;
+const DOMPurifyServer = DOMPurify(window);
 
 
 export default async function page({params}: {params: {id : string}}) {
@@ -20,7 +25,7 @@ export default async function page({params}: {params: {id : string}}) {
     <div className='w-full lg:mx-20 lg:my-10'>
       <div className='lg:flex md:flex-none'>
         <div className='md:w-full lg:w-2/3 lg:border-r-2'>
-        <div dangerouslySetInnerHTML={renderHTML(blog.title)}></div>
+        <div dangerouslySetInnerHTML={{ __html: DOMPurifyServer.sanitize(blog.title) }}></div>
           <div className='text-sm md:flex-none lg:flex gap-4 my-2 mx-5'>
             <Image src={blog.createdBy.image || Pic} alt='AuthorPic' className='h-16 w-16 rounded-full' width={400} height={400}></Image>
             <div className='items-center justify-center'>
@@ -30,7 +35,7 @@ export default async function page({params}: {params: {id : string}}) {
           </div>
           <div className='mx-5 my-5'>
           <Image src={blog.poster || Banner} alt='BlogBanner' className='p-5 w-full h-96 object-cover' width={1500} height={1500}></Image>
-          <div dangerouslySetInnerHTML={renderHTML(blog.desc)}></div>
+          <div dangerouslySetInnerHTML={{ __html: DOMPurifyServer.sanitize(blog.desc) }}></div>
 
           </div>
         </div>
