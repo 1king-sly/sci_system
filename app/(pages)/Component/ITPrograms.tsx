f18e62afb7657975a2d33799023b6d1d64f2a684
ClarenceLaria <clarencelaria@gmail.com>
2024-03-19 12:45:28 +0300

    ITDepartment Page

---
+++ app/(pages)/Component/ITPrograms.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Programs() {
+  return (
+    <div id="Programmes" className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
+    <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+        PROGRAMMES WE OFFER  
+        </h2>
+    </div>
+    <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+        <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+            <svg className="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+            </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Bachelor of Science (Information Technology)</h6>
+            <p className="mb-3 text-sm text-gray-900">
+                A Brief Description About The Program
+            </p>
+        </div>
+        </div>
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+        <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+            <svg className="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+            </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Bachelor Of Science in Information Science</h6>
+            <p className="mb-3 text-sm text-gray-900">
+                A Brief Description About The Program
+            </p>
+        </div>
+        </div>
+    
+    </div>
+    </div>
+  )
+}
