f02b3c354e068555bbf784fd18e7ad9f4f762587
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-03-21 10:19:56 +0300

    integrated rendering of user styles on text in lbogs

--- app/(pages)/adminstrator/page.tsx
+++ app/(pages)/adminstrator/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 import React from 'react'
 import { usePathname } from 'next/navigation'
+import ProfileForm from '../Component/ProfileForm'
+import Loading from '../loading'
 
 export default function Page() {
   const pathName = usePathname()
+
+  if(pathName === '/adminstrator'){
+    return <Loading/>
+  }
+
   return (
     <div>{pathName}</div>
   )
