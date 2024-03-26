3507109472f4d9531ce9e5fb8f0af9b0edf3f39e
ClarenceLaria <clarencelaria@gmail.com>
2024-03-26 09:17:07 +0300

    SIK Page

---
+++ app/(pages)/ITDepartment/SIK/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full mx-20 my-10'>
+        <h1 className='text-xl font-bold my-3'>Bachelor of Science in Information Technology</h1>
+        <h1 className='text-xl font-bold my-3'>About the Course</h1>
+        <p className=''>Data, information, and knowledge are increasingly recognized as a crucial asset in modern organizations. The course intends to bring the students a solid general understanding of data, information and knowledge management focusing on concepts, methods and tools that can be used to enable innovation and change management within the organizations. A particular focus will be proposed on service science, an interdisciplinary approach to the study, design, and implementation of services systems. Organizations are complex systems in which specific interdependencies of people and technologies take actions and provide value.
+            The course enable the student to deeply analyse from an organisational and managerial perspective Information and Knowledge management systems, Information system and data management, double side platforms, and some emerging technological phenomena such as crowdsourcing and gamification.
+        </p>
+        <h1 className='my-3'><span className='font-bold'>Course duration:</span> Four (4) academic years</h1>
+        <h1 className='font-bold'>Minimum Entry Requirements</h1>
+        <h1>Mean grade of C+ (plus) at KCSE with C+ (plus) in Mathematics,
+            in any science or any business related subject. or, 2 principal
+            passes at A-level, 1 of which must be in Mathematics or
+            Science subject, or Any business related subject, or Those
+            with a diploma in IT from MMUST or equivalent with a credit
+            or distinction will be admitted into 2nd year of study. or a
+            relevant diploma recognized by the University Senate</h1>
+        {/* <h1 className='text-xl font-bold my-3'>Course Requirements</h1> */}
+    </div>
+  )
+
+
+}
--- app/(pages)/ITDepartment/informationTechnology/page.tsx
+++ app/(pages)/ITDepartment/informationTechnology/page.tsx
@@ -10,7 +10,17 @@ export default function page() {
             The degree is a Bachelor of Science degree with institutions conferring degrees in the fields of information technology and related fields. This degree is awarded for completing a program of study in the field of software development, software testing, software engineering, web design, databases, programming, computer networking and computer systems.
             Graduates with an information technology background are able to perform technology tasks relating to the processing, storing, and communication of information between computers, mobile phones, and other electronic devices. Information technology as a field emphasizes the secure management of large amounts of variable information and its accessibility via a wide variety of systems both local and worldwide.
         </p>
-        <h1 className='text-xl font-bold my-3'>Course Requirements</h1>
+        <h1 className='my-3'><span className='font-bold'>Course duration:</span> Four (4) academic years</h1>
+        <h1 className='font-bold'>Minimum Entry Requirements</h1>
+        <h1>Mean grade of C+ (plus) at KCSE with C+ (plus) in Mathematics,
+          Physics or physical sciences 2 principal passes at A-level, 1
+          of which must be in Mathematics or Science subject, Or Any
+          business related subject or Those with a diploma in IT from
+          MMUST or equivalent with a credit or distinction will be
+          admitted into 2nd year of study. or a relevant diploma from a
+          recognized institution.</h1>        {/* <h1 className='text-xl font-bold my-3'>Course Requirements</h1> */}
     </div>
   )
+
+
 }
