43804f5a6fab61e9f9e0ce4d12c22355d664011b
ClarenceLaria <clarencelaria@gmail.com>
2024-03-27 13:06:14 +0300

    MSCCS page

---
+++ app/(pages)/department/mscCS/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full mx-20 my-10'>
+        <h1 className='text-xl font-bold my-3'>Masters of Science in Computer Science</h1>
+        <h1 className='text-xl font-bold my-3'>About the Course</h1>
+        <p className=''>
+          The Master of Science in Computer Science (MSCS) program is designed to provide students with advanced knowledge and skills in the field of computer science. 
+          This graduate-level program offers a comprehensive curriculum covering a wide range of topics, including algorithms, software engineering, artificial intelligence, data science, and computer networking. 
+          Through a combination of coursework, research projects, and practical experiences, students gain expertise in both theoretical foundations and practical applications of computer science. 
+          The MSCS program prepares graduates for careers in various industries, including technology, finance, healthcare, and academia, by fostering critical thinking, problem-solving, and innovation in the rapidly evolving field of computer science.        
+        </p>
+        <h1 className='my-3'><span className='font-bold'>Course duration:</span> Four (4) academic years</h1>
+        <h1 className='font-bold'>Minimum Entry Requirements</h1>
+        <h1>Relevant Bachelor's Degree from a recognised University with Second Class Upper or Second Class Lower with two(2) years relevant experience</h1>
+    </div>
+  )
+
+
+}
