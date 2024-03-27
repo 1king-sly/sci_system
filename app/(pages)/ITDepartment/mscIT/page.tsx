cc76fd8fd3d30f4757cf651927b521427a3f88d5
ClarenceLaria <clarencelaria@gmail.com>
2024-03-27 12:53:35 +0300

    MSCIT and PHDIT pages

---
+++ app/(pages)/ITDepartment/mscIT/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full mx-20 my-10'>
+        <h1 className='text-xl font-bold my-3'>Mastes of Science in Information Technology</h1>
+        <h1 className='text-xl font-bold my-3'>About the Course</h1>
+        <p className=''>
+            The Master of Science in Information Technology (MSIT) program is designed to equip students with advanced knowledge and skills in the field of information technology (IT). 
+            This graduate-level program focuses on preparing students for leadership roles in various sectors, including business, government, healthcare, and academia. Through a combination of coursework and practical experience, students gain expertise in areas such as software development, cybersecurity, data analytics, networking, and IT management. 
+            The MSIT program emphasizes problem-solving, critical thinking, and innovation, empowering graduates to address complex technological challenges and drive organizational success in an increasingly digital world.        
+        </p>
+        <h1 className='my-3'><span className='font-bold'>Course duration:</span> Four (4) academic years</h1>
+        <h1 className='font-bold'>Minimum Entry Requirements</h1>
+        <h1>Relevant Bachelor's Degree from a recognised University with Second Class Upper or Second Class Lower with two(2) years relevant experience</h1>
+    </div>
+  )
+
+
+}
---
+++ app/(pages)/ITDepartment/phdIT/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full mx-20 my-10'>
+        <h1 className='text-xl font-bold my-3'>Doctor of Philosophy in Information Technology</h1>
+        <h1 className='text-xl font-bold my-3'>About the Course</h1>
+        <p className=''>
+            The Doctor of Philosophy (PhD) in Information Technology program is a rigorous doctoral-level program that prepares students for careers in research, academia, and leadership roles within the field of information technology (IT). 
+            This program offers advanced study and research opportunities in areas such as computer science, cybersecurity, data science, network engineering, and information systems management. Students engage in independent research projects, publish scholarly articles, and collaborate with faculty mentors to advance knowledge and innovation in IT. 
+            The PhD in Information Technology program emphasizes critical thinking, problem-solving, and interdisciplinary collaboration, equipping graduates with the skills and expertise to address complex challenges in technology and contribute to the advancement of the field through original research.            
+        </p>
+        <h1 className='my-3'><span className='font-bold'>Course duration:</span> Four (4) academic years</h1>
+        <h1 className='font-bold'>Minimum Entry Requirements</h1>
+        <h1>Relevant Masters Degree from a recognised University</h1>
+    </div>
+  )
+
+
+}
