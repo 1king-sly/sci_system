be4db3284cd8b90074751d8df9d2769d4ddebc6c
ClarenceLaria <clarencelaria@gmail.com>
2024-03-28 08:51:49 +0300

    Completed Programs Page

---
+++ app/(pages)/department/bscCF/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full mx-20 my-10'>
+        <h1 className='text-xl font-bold my-3'>Bachelor of Science in Computer Security and Forensics</h1>
+        <h1 className='text-xl font-bold my-3'>About the Course</h1>
+        <p className=''>
+            The Bachelor of Science in Computer Forensics (BSCF) program is a specialized undergraduate degree that focuses on the investigation, analysis, and prevention of cybercrimes and digital security breaches. 
+            Students in this program learn to apply forensic techniques and tools to gather, preserve, and analyze digital evidence from computers, networks, and other digital devices. The curriculum covers topics such as data recovery, network security, cyber law, ethical hacking, and incident response. 
+            Through hands-on lab exercises and real-world case studies, students develop skills in digital forensics, cybersecurity, and risk management. 
+            Graduates of the BSCF program are prepared for careers as forensic analysts, cybersecurity specialists, law enforcement officers, and IT security consultants, helping organizations protect sensitive information and combat cyber threats in an increasingly digital world.
+        </p>
+        <h1 className='my-3'><span className='font-bold'>Course duration:</span> Four (4) academic years</h1>
+        <h1 className='font-bold'>Minimum Entry Requirements</h1>
+        <h1>Mean grade of C+ (plus) at KCSE and a C+ (plus) in
+            Mathematics, Physics or Physical Sciences. or 2 Principal
+            passes in relevant science subjects, 1 of which must be in
+            Mathematics or Physics at A-level. or Diploma in Computer
+            Studies or its equivalent (may be exempted in relevant units)</h1>
+    </div>
+  )
+
+
+}
