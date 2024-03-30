75050fad60949db711fd488dabb3655d58caa69c
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-03-30 03:54:34 +0300

    creating research backend and frontend logic implemented

--- app/(pages)/research/urf/page.tsx
+++ app/(pages)/research/urf/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import CreateResearch from '../Components/CreateResearch'
 
 export default function page() {
   return (
-    <div>
-        <p>
-            Step into the world of university research funding, where innovation and exploration are nurtured. University research funds serve as vital resources that propel cutting-edge research initiatives within academic institutions. 
-            These funds are tailored to support the diverse array of research projects conducted by faculty, researchers, and students across various disciplines. From groundbreaking scientific discoveries to insightful social science inquiries and innovative technological advancements, university research funding fuels the pursuit of knowledge and fosters academic excellence. Join us as we delve into the dynamic landscape of university research funding and uncover the transformative impact it has on shaping the future of education and scholarship.                        
-        </p>
-    </div>
+   <>
+   <CreateResearch type='URF'/>
+   </>
   )
 }
+
+
+
