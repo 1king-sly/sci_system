dea94afed2a63581fca9563d68a3317d2a1b05ea
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-05-20 12:37:35 +0300

    rectified routing

--- app/(pages)/Component/Cards.tsx
+++ app/(pages)/Component/Cards.tsx
@@ -12,17 +12,17 @@ export default function Cards() {
               </h1>
         </div>
         </Link> 
-        <Link href="/department">
+        <Link href="/research">
           <div id="Department" className=" cursor-pointer px-10 py-20 text-center transition duration-300 transform bg-gray-900 rounded shadow-2xl hover:scale-105 md:shadow-xl hover:shadow-2xl">
             <h1 className="font-semibold text-gray-200 text-4xl">
-                  Department
+                  Researchs
               </h1>
         </div></Link>
         
-        <Link href="/club">
+        <Link href="/blog">
         <div id="Clubs" className=" cursor-pointer px-10 py-20 text-center transition duration-300 transform bg-gray-900 rounded shadow-2xl hover:scale-105 md:shadow-xl hover:shadow-2xl">
             <h1 className="font-semibold text-gray-200 text-4xl">
-               Clubs
+               Blog
               </h1>
         </div></Link>
         <Link href="/events">
